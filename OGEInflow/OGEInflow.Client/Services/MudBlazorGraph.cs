6c950c62b4373ab1ec0047af4cee6f187cfab0ab
aurialt <144707111+aurialt@users.noreply.github.com>
2025-03-30 23:50:37 -0500

    Added Pie Graph methods

--- OGEInflow/OGEInflow.Client/Services/MudBlazorGraph.cs
+++ OGEInflow/OGEInflow.Client/Services/MudBlazorGraph.cs
@@ -5,13 +5,13 @@ public class MudBlazorGraph
 {
     public ChartOptions Options { get; set; }
     public List<ChartSeries> Series { get; set; }
-    public string[] XAxisLabels { get; set; }
+    public string[] Labels { get; set; }
     public Dictionary<string, int> EventCounts { get; set; }
 
-    public MudBlazorGraph(ChartOptions options, string[] xAxisLabels, Dictionary<string, int> eventCounts, List<ChartSeries> series)
+    public MudBlazorGraph(ChartOptions options, string[] labels, Dictionary<string, int> eventCounts, List<ChartSeries> series)
     {
         Options = options;
-        XAxisLabels = xAxisLabels;
+        Labels = labels;
         EventCounts = eventCounts;
         Series = series;
     }
@@ -28,7 +28,31 @@ public class MudBlazorGraph
 
         return new MudBlazorGraph(options, labels, eventCounts, series);
     }
+    
+    public static List<ChartSeries> CreatePieSeries(Dictionary<string, int> eventCounts)
+    {
+        return new List<ChartSeries>
+        {
+            new ChartSeries
+            {
+                Name = "Pie Chart Data",  // Optional, could be used for the chart's legend
+                Data = eventCounts.Values.Select(count => (double)count).ToArray(),
+            }
+        };
+    }
+    
+    public static MudBlazorGraph CreateGraphForPieChart(
+        Dictionary<string, List<ReaderEvent>> sourceData,
+        string[]? xAxisLabels,
+        ChartOptions options)
+    {
+        // Create the event counts and pie chart series
+        Dictionary<string, int> eventCounts = ComputeEventCounts(sourceData, xAxisLabels ?? ExtractLabels(sourceData));
+        List<ChartSeries> series = CreatePieSeries(eventCounts);
 
+        // Return a MudBlazorGraph instance with pie chart data
+        return new MudBlazorGraph(options, xAxisLabels ?? ExtractLabels(sourceData), eventCounts, series);
+    }
     
     public static string[] ExtractLabels(Dictionary<string, List<ReaderEvent>> sourceData)
     {
