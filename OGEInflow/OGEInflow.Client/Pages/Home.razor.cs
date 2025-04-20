6ebd71c43b988da1a246ac1d8a17e697e45223da
aurialt <144707111+aurialt@users.noreply.github.com>
2025-04-19 21:01:07 -0500

    Fixed performance issues in Home/Activity Page (66% faster!)

--- OGEInflow/OGEInflow.Client/Pages/Home.razor.cs
+++ OGEInflow/OGEInflow.Client/Pages/Home.razor.cs
@@ -1,240 +1,218 @@
 using OGEInflow.Services;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
+using System.Threading.Tasks;
 
-namespace OGEInflow.Client.Pages;
-
-public partial class Home : ComponentBase
+namespace OGEInflow.Client.Pages
 {
-    /* Graphs Section */
-    public static MudBlazorGraph ScanActivationsGraph,
-        RankedPersonIDGraph,
-        RankedReaderIDGraph,
-        RankedMachineGraph,
-        RankedAvgAreaGraph;
-
-    private static MudDatePicker StartPicker;
-    private static MudDatePicker EndPicker;
-
-    //Date Boundaries for the file given
-    private static DateTime? FirstDate => ReaderEvent.MinDate;
-    private static DateTime? LastDate => ReaderEvent.MaxDate;
-    
-    private static int dateRange;
-
-    //Dates user can filter (Date Bounds)
-    private static DateTime? StartDate { get; set; }
-    private static DateTime? EndDate { get; set; }
-    private static bool _autoClose = false;
-
-    protected override void OnInitialized()
+    public partial class Home : ComponentBase
     {
-        if (ReaderEvent.readerEventsList != null)
+        /* Graphs Section */
+        public static MudBlazorGraph ScanActivationsGraph,
+            RankedPersonIDGraph,
+            RankedReaderIDGraph,
+            RankedMachineGraph,
+            RankedAvgAreaGraph;
+
+        private static MudDatePicker StartPicker;
+        private static MudDatePicker EndPicker;
+
+        private static DateTime? FirstDate => ReaderEvent.MinDate;
+        private static DateTime? LastDate => ReaderEvent.MaxDate;
+
+        private static int dateRange;
+
+        private static DateTime? StartDate { get; set; }
+        private static DateTime? EndDate { get; set; }
+        private static bool _autoClose = false;
+
+        private static List<ReaderEvent> filteredReaderEvents;
+
+        protected override async Task OnInitializedAsync()
         {
-            LoadGraphs();
-        }
-    }
-
-
-    public static void InitializeDateBounds(DateTime startDate, DateTime endDate)
-    {
-        StartDate = startDate;
-        EndDate = endDate;
-    }
-
-    public void RefreshGraphs()
-    {
-        LoadGraphs();
-        StateHasChanged();
-        Console.WriteLine(" (LoadGraphs) Date Range: " + dateRange);
-    }
-
-    //Will cause page error if all create graphs aren't put into here
-    private static void LoadGraphs()
-    {
-        if (StartDate != null && EndDate != null)
-        {
-            TimeSpan diff = EndDate.Value - StartDate.Value;
-            dateRange = (int)diff.TotalDays;
+            if (ReaderEvent.readerEventsList != null)
+            {
+                await LoadGraphsAsync();
+            }
         }
 
-        createRankedAvgAreaGraph();
-        createScanActivationGraph();
-        createRankedPersonIDGraph();
-        createRankedReaderIDGraph();
-        // createRankedDevIDGraph();
-        createRankedMachineGraph();
-    }
-    
-    
-    
-    /* Graph Creation Functions */
-    private static void createScanActivationGraph()
-    {
-        ChartOptions options = new ChartOptions();
-
-        string[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-
-        var sortedDayOfWeekReaderEvents = ReaderEvent.DayOfWeekReaderEventsDict
-            .Where(entry => daysOfWeek.Contains(entry.Key))
-            .OrderBy(entry => Array.IndexOf(daysOfWeek, entry.Key))
-            .ToList();
-
-        List<ChartSeries> series = new List<ChartSeries>
+        public static void InitializeDateBounds(DateTime startDate, DateTime endDate)
         {
-            new()
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public void RefreshGraphs()
+        {
+            if (ReaderEvent.readerEventsList == null) return;
+
+            LoadGraphsAsync();
+            StateHasChanged();
+            Console.WriteLine(" (LoadGraphs) Date Range: " + dateRange);
+        }
+
+        private static async Task LoadGraphsAsync()
+        {
+            if (StartDate != null && EndDate != null)
             {
-                Name = "Scan Activations per Day",
-                Data = sortedDayOfWeekReaderEvents
-                    .Select(entry => (double)entry.Value.Count)
-                    .ToArray()
+                TimeSpan diff = EndDate.Value - StartDate.Value;
+                dateRange = (int)diff.TotalDays;
             }
-        };
 
-        ScanActivationsGraph =
-            MudBlazorGraph.CreateGraph(series, ReaderEvent.DayOfWeekReaderEventsDict, daysOfWeek, options);
-    }
+            FilterReaderEvents();
 
-    private static void createRankedPersonIDGraph()
-    {
-        //Gets Top 5 Scan Activations
-        var rankedPersonIDGraph = GetTopRankedEventsFiltered(ReaderEvent.PersonIDDict, 5);
-        
-        //TESTING SECTION
-        // Console.WriteLine("RankedPersonIDGraph FILTERED DICTIONARY ");
-        // int rank = 1;
-        // foreach (var entry in rankedPersonIDGraph)
-        // {
-        //     Console.WriteLine($"Rank #{rank}");
-        //     Console.WriteLine($"PersonID: {entry.Key}");
-        //     Console.WriteLine($"Event Count: {entry.Value.Count}");
-        //
-        //     foreach (var readerEvent in entry.Value)
-        //     {
-        //         Console.WriteLine($" - EventTime: {readerEvent.EventTime}");
-        //         // You can print other properties of readerEvent here as needed
-        //     }
-        //
-        //     rank++;
-        // }
+            await Task.WhenAll(
+                createRankedAvgAreaGraphAsync(),
+                createScanActivationGraphAsync(),
+                createRankedPersonIDGraphAsync(),
+                createRankedReaderIDGraphAsync(),
+                createRankedMachineGraphAsync()
+            );
+        }
 
-        Console.WriteLine("FORLOOP END - RankedPersonIDGraph FILTERED DICTIONARY ");
-
-        ChartOptions options = new ChartOptions();
-
-        List<ChartSeries> series = new List<ChartSeries>
+        private static void FilterReaderEvents()
         {
-            new()
+            filteredReaderEvents = ReaderEvent.readerEventsList
+                .Where(re =>
+                    DateTime.TryParse(re.EventTime, out DateTime eventDate) &&
+                    eventDate >= StartDate.Value && eventDate <= EndDate.Value)
+                .ToList();
+        }
+
+        private static Dictionary<string, List<ReaderEvent>> GroupEventsByKey(Func<ReaderEvent, string> keySelector)
+        {
+            return filteredReaderEvents
+                .GroupBy(keySelector)
+                .ToDictionary(g => g.Key, g => g.ToList());
+        }
+
+        private static Task createScanActivationGraphAsync()
+        {
+            ChartOptions options = new ChartOptions();
+
+            string[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            var sortedDayOfWeekReaderEvents = ReaderEvent.DayOfWeekReaderEventsDict
+                .Where(entry => daysOfWeek.Contains(entry.Key))
+                .OrderBy(entry => Array.IndexOf(daysOfWeek, entry.Key))
+                .ToList();
+
+            List<ChartSeries> series = new List<ChartSeries>
             {
-                Name = "Top 5 People",
-                Data = rankedPersonIDGraph.Values
-                    .Select(entry => (double)entry.Count)
-                    .ToArray()
-            }
-        };
+                new()
+                {
+                    Name = "Scan Activations per Day",
+                    Data = sortedDayOfWeekReaderEvents
+                        .Select(entry => (double)entry.Value.Count)
+                        .ToArray()
+                }
+            };
 
-        RankedPersonIDGraph = MudBlazorGraph.CreateGraph(series, rankedPersonIDGraph, null, options);
-        // ReaderEventWarning.CheckDoubleScans(ReaderEvent.PersonIDDict);
-    }
+            ScanActivationsGraph = MudBlazorGraph.CreateGraph(series, ReaderEvent.DayOfWeekReaderEventsDict, daysOfWeek, options);
+            return Task.CompletedTask;
+        }
 
-
-    public static void createRankedReaderIDGraph()
-    {
-        var rankedReaderIDGraph = GetTopRankedEventsFiltered(ReaderEvent.ReaderIDDict, 5);
-        
-        ChartOptions options = new ChartOptions();
-    
-        List<ChartSeries> series = new List<ChartSeries>
+        private static Task createRankedPersonIDGraphAsync()
         {
-            new()
+            var personIDDict = GroupEventsByKey(re => re.ID);
+            var rankedPersonIDGraph = GetTopRanked(personIDDict, 5);
+
+            ChartOptions options = new ChartOptions();
+
+            List<ChartSeries> series = new List<ChartSeries>
             {
-                Name = "Top 5 Readers (Based on ReaderID)",
-                Data = rankedReaderIDGraph.Values
-                    .Select(entry => (double)entry.Count)
-                    .ToArray()
-            }
-        };
-        
-        RankedReaderIDGraph = MudBlazorGraph.CreateGraph(series, rankedReaderIDGraph, null, options);
-        ReaderEventWarning.CheckTooManyReaderScans(rankedReaderIDGraph,5);
-    }
+                new()
+                {
+                    Name = "Top 5 People",
+                    Data = rankedPersonIDGraph.Values
+                        .Select(entry => (double)entry.Count)
+                        .ToArray()
+                }
+            };
 
-    public static void createRankedMachineGraph()
-    {
-        var rankedMachineGraph = GetTopRankedEventsFiltered(ReaderEvent.MachineDict, 5);
-        
-        ChartOptions options = new ChartOptions();
-    
-        List<ChartSeries> series = new List<ChartSeries>
+            RankedPersonIDGraph = MudBlazorGraph.CreateGraph(series, rankedPersonIDGraph, null, options);
+            return Task.CompletedTask;
+        }
+
+        private static Task createRankedReaderIDGraphAsync()
         {
-            new()
+            var readerIDDict = GroupEventsByKey(re => re.ReaderID);
+            var rankedReaderIDGraph = GetTopRanked(readerIDDict, 5);
+
+            ChartOptions options = new ChartOptions();
+
+            List<ChartSeries> series = new List<ChartSeries>
             {
-                Name = "Top 5 Panels (Based on Machine)",
-                Data = rankedMachineGraph.Values
-                    .Select(entry => (double)entry.Count)
-                    .ToArray()
-            }
-        };
-        
-        RankedMachineGraph = MudBlazorGraph.CreateGraph(series, rankedMachineGraph, null, options);
-    }
+                new()
+                {
+                    Name = "Top 5 Readers (Based on ReaderID)",
+                    Data = rankedReaderIDGraph.Values
+                        .Select(entry => (double)entry.Count)
+                        .ToArray()
+                }
+            };
 
-    public static void createRankedAvgAreaGraph()
-    {
-        var rankedAvgAreaGraph = GetTopRankedEventsFiltered(ReaderEvent.ReaderDescDict, 5);
+            RankedReaderIDGraph = MudBlazorGraph.CreateGraph(series, rankedReaderIDGraph, null, options);
+            return Task.CompletedTask;
+        }
 
-        ChartOptions options = new ChartOptions
+        private static Task createRankedMachineGraphAsync()
         {
-            ChartPalette = new[] { "#4EE1EC" }, // bright blue
-            XAxisLines = true,
-            YAxisLines = false,
-            
-            
-        };
-    
-        List<ChartSeries> series = new List<ChartSeries>
-        {
-            new()
+            var machineDict = GroupEventsByKey(re => re.MACHINE);
+            var rankedMachineGraph = GetTopRanked(machineDict, 5);
+
+            ChartOptions options = new ChartOptions();
+
+            List<ChartSeries> series = new List<ChartSeries>
             {
-                Name = "Top 5 Areas (Based on ReaderDesc)",
-                Data = rankedAvgAreaGraph.Values
-                    .Select(entry => (double)entry.Count / dateRange)
-                    .ToArray(),
-                ShowDataMarkers = false
-                
-            }
-        };
-        
-        RankedAvgAreaGraph = MudBlazorGraph.CreateGraph(series, rankedAvgAreaGraph, null, options);
+                new()
+                {
+                    Name = "Top 5 Panels (Based on Machine)",
+                    Data = rankedMachineGraph.Values
+                        .Select(entry => (double)entry.Count)
+                        .ToArray()
+                }
+            };
+
+            RankedMachineGraph = MudBlazorGraph.CreateGraph(series, rankedMachineGraph, null, options);
+            return Task.CompletedTask;
+        }
+
+        private static Task createRankedAvgAreaGraphAsync()
+        {
+            var readerDescDict = GroupEventsByKey(re => re.ReaderDesc);
+            var rankedAvgAreaGraph = GetTopRanked(readerDescDict, 5);
+
+            ChartOptions options = new ChartOptions
+            {
+                ChartPalette = new[] { "#4EE1EC" },
+                XAxisLines = true,
+                YAxisLines = false
+            };
+
+            List<ChartSeries> series = new List<ChartSeries>
+            {
+                new()
+                {
+                    Name = "Top 5 Areas (Based on ReaderDesc)",
+                    Data = rankedAvgAreaGraph.Values
+                        .Select(entry => (double)entry.Count / dateRange)
+                        .ToArray(),
+                    ShowDataMarkers = false
+                }
+            };
+
+            RankedAvgAreaGraph = MudBlazorGraph.CreateGraph(series, rankedAvgAreaGraph, null, options);
+            return Task.CompletedTask;
+        }
+
+        private static Dictionary<string, List<ReaderEvent>> GetTopRanked(Dictionary<string, List<ReaderEvent>> inputDict, int topCount)
+        {
+            return inputDict
+                .Where(entry => entry.Value.Any())
+                .OrderByDescending(entry => entry.Value.Count)
+                .Take(topCount)
+                .ToDictionary(entry => entry.Key, entry => entry.Value);
+        }
     }
-
-    
-    
-
-    /* Helper functions */
-    public static Dictionary<string, List<ReaderEvent>> GetTopRankedEventsFiltered(Dictionary<string, List<ReaderEvent>> inputDict, int topCount)
-    {
-        var rankedDict = inputDict
-            .ToDictionary(
-                entry => entry.Key,
-                entry => entry.Value
-                    .Where(re =>
-                    {
-                        if (DateTime.TryParse(re.EventTime, out DateTime eventDate))
-                        {
-                            return eventDate >= StartDate.Value && eventDate <= EndDate.Value;
-                        }
-                        return false;
-                    })
-                    .ToList()
-            )
-            .Where(entry => entry.Value.Any())
-            .OrderByDescending(entry => entry.Value.Count)
-            .Take(topCount)
-            .ToDictionary(entry => entry.Key, entry => entry.Value);
-
-        return rankedDict;
-    }
-    
-
 }
