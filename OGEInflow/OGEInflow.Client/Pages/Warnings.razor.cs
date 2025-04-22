df9592334e9dbfd8b1d10575e9e2812a9fdead1b
aurialt <at1007641@student.francistuttle.edu>
2025-04-22 11:01:16 -0500

    Added temporary table to Warnings

--- OGEInflow/OGEInflow.Client/Pages/Warnings.razor.cs
+++ OGEInflow/OGEInflow.Client/Pages/Warnings.razor.cs
@@ -1,7 +1,12 @@
 using Microsoft.AspNetCore.Components;
+using OGEInflow.Services;
 
 namespace OGEInflow.Client.Pages;
 
 public partial class Warnings : ComponentBase
 {
+
+    private void addDoubleScanWarning(ReaderEvent re)
+    {
+    }
 }
