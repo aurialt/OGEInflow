a485bba8aa6587c365fd31976ece2e6c9ffe1ff9
aurialt <144707111+aurialt@users.noreply.github.com>
2025-05-04 00:48:02 -0500

    Created Method for overwriting popup, refactoring

--- OGEInflow/OGEInflow.Client/Components/Pin.razor.cs
+++ OGEInflow/OGEInflow.Client/Components/Pin.razor.cs
@@ -4,29 +4,18 @@ namespace OGEInflow.Client.Components;
 
 public partial class Pin : ComponentBase
 {
-    [Parameter]
-    public string Top { get; set; } = "0px";
+    [Parameter] public string Top { get; set; } = "0px";
+    [Parameter] public string Left { get; set; } = "0px";
 
-    [Parameter]
-    public string Left { get; set; } = "0px";
+    [Parameter] public string Bottom { get; set; } = "0px";
 
-    [Parameter] 
-    public string Bottom { get; set; } = "0px";
+    [Parameter] public string Right { get; set; } = "0px";
 
-    [Parameter]
-    public string Right { get; set; } = "0px";
+    [Parameter] public string PinColor { get; set; } = "rgb(42, 121, 19);";
+    [Parameter] public string Label { get; set; } = "";
+    [Parameter] public string Class { get; set; } = "";
 
-    [Parameter]
-    public string PinColor { get; set; } = "rgb(42, 121, 19);";
-
-    [Parameter]
-    public string Label { get; set; } = "";
-
-    [Parameter]
-    public string Class { get; set; } = "";
-
-    [Parameter]
-    public Action OnClick { get; set; }
+    [Parameter] public Action OnClick { get; set; }
 
     public static bool UseGradientColor { get; set; } = false;
     private string ComputedPinColor => UseGradientColor ? GetColorFromValue(0) : PinColor;
--- OGEInflow/OGEInflow.Client/Pages/Map.razor.cs
+++ OGEInflow/OGEInflow.Client/Pages/Map.razor.cs
@@ -7,6 +7,11 @@ namespace OGEInflow.Client.Pages;
 
 public partial class Map : ComponentBase
 {
+    public string IconName { get; set; } = "";
+    public string Usage { get; set; } = "";
+    public List<string> Issues { get; set; } = new();
+    public List<string> ConnectedReaders { get; set; } = new();
+    
     private bool ShowPopup {get; set;} = false;
     
     private int testoutput = 0;
@@ -14,18 +19,31 @@ public partial class Map : ComponentBase
     private  void TestOutputVal()
     {
         testoutput++;
-        ToggleColor();
+        ToggleGradientColor();
     }
     
-    private async Task ToggleColor()
+    private void ToggleGradientColor()
     {
         Pin.UseGradientColor = !Pin.UseGradientColor;
         StateHasChanged();   // force re-render to update style
+    }
+
+    private void EditPopup(string iconName, string usage, List<string>? issues, List<string>? connectedReaders)
+    {
+        IconName = iconName;
+        Usage = usage;
+        if (issues is not null)
+        Issues = issues;
+        if (connectedReaders is not null)
+        ConnectedReaders = connectedReaders;
+        TogglePopup();
     }
 
     private void TogglePopup()
     {
         ShowPopup = !ShowPopup;
     }
+    
+    
 
 }
