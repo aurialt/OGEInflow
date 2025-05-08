8c61ee19fd68071a142a35a3a27a74d7e1cc44f8
aurialt <144707111+aurialt@users.noreply.github.com>
2025-05-07 22:16:22 -0500

    Added all warnings to activity page, temporarily removed location filtering

--- OGEInflow/OGEInflow.Client/Pages/Warnings.razor.cs
+++ OGEInflow/OGEInflow.Client/Pages/Warnings.razor.cs
@@ -4,31 +4,4 @@ namespace OGEInflow.Client.Pages;
 
 public partial class Warnings : ComponentBase
 {
-    
-
-// Quick filter for Double Scan (based on Warning properties)
-    private string _searchStringDoubleScans;
-    private Func<Warning, bool> quickFilterDoubleScan => x =>
-    {
-        if (string.IsNullOrWhiteSpace(_searchStringDoubleScans))
-            return true;
-
-        return
-            (x.ReaderId?.Contains(_searchStringDoubleScans, StringComparison.OrdinalIgnoreCase) ?? false) ||
-            (x.Message?.Contains(_searchStringDoubleScans, StringComparison.OrdinalIgnoreCase) ?? false) ||
-            x.Timestamp.ToString().Contains(_searchStringDoubleScans);
-    };
-
-// Quick filter for High Reader Usage
-    private string _searchStringHighUsageScans;
-    private Func<Warning, bool> quickFilterHighUsageScans => x =>
-    {
-        if (string.IsNullOrWhiteSpace(_searchStringHighUsageScans))
-            return true;
-
-        return
-            (x.ReaderId?.Contains(_searchStringHighUsageScans, StringComparison.OrdinalIgnoreCase) ?? false) ||
-            (x.Message?.Contains(_searchStringHighUsageScans, StringComparison.OrdinalIgnoreCase) ?? false) ||
-            x.Timestamp.ToString().Contains(_searchStringHighUsageScans);
-    };
 }
